50ff99329ed2f90e11c343a6a9db2bd8e6e0f560
da <pridaproeungso@gmail.com>
2023-08-05 22:18:48 +0700

    initial load

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// // internal class Program
+// // {
+// //     private static void Main(string[] args)
+// //     {
+// //         Console.WriteLine("Hello, World!");
+// //     }
+// // }
+
+// using System;
+
+// namespace MyApp // Note: actual namespace depends on the project name.
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+using consoletest;
+using System;
+namespace myapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine ("Hi world!");
+        }
+
+    }
+}
+// Console.WriteLine ("Hello world!");
+// var usrer = new User();
